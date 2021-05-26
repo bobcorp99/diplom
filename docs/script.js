cf5131a7022b78565ba8fad47edd50adf9a95897
bobcorp <bobcorp99@mail.ru>
2021-05-26 19:59:47 +0500

    version 1.01

---
+++ docs/script.js
@@ -0,0 +1,127 @@
+const product = {
+    plainBurger: {
+        name: 'MATO',
+        price: 10000,
+        amount: 0,
+        calory: 1,
+        get sum() {
+            return this.amount * this.price
+        },
+        get kcall() {
+            return this.amount * this.calory
+        }
+    },
+    freshBurger: {
+        name: 'TEMIR PROFIL',
+        price: 20500,
+        amount: 0,
+        calory: 1,
+        get sum() {
+            return this.amount * this.price
+        },
+        get kcall() {
+            return this.amount * this.calory
+        }
+    },
+    freshCombo: {
+        name: 'TAXTA',
+        price: 31900,
+        amount: 0,
+        calory: 1,
+        get sum() {
+            return this.amount * this.price
+        },
+        get kcall() {
+            return this.amount * this.calory
+        }
+    },
+}
+
+const btnPlusOrMinus = document.querySelectorAll('.main__product-btn'),
+    delivery = document.querySelector('.addCart')
+
+
+for (let i = 0; i < btnPlusOrMinus.length; i++) {
+    btnPlusOrMinus[i].addEventListener('click', function () {
+        addOrSubtract(this)
+    })
+}
+
+function addOrSubtract(element) {
+    const parent = element.closest('.main__product')
+    const parentId = parent.getAttribute('id')
+    const elementSymbol = element.getAttribute('data-symbol')
+    const output = parent.querySelector('.main__product-num')
+    const price = parent.querySelector('.main__product-price span')
+    const kcall = parent.querySelector('.main__product-kcall span')
+
+    if (elementSymbol == '+') {
+        product[parentId].amount++
+    } else if (elementSymbol == '-' && product[parentId].amount > 0) {
+        product[parentId].amount--
+    }
+
+    output.innerHTML = product[parentId].amount
+    price.innerHTML = product[parentId].sum
+    kcall.innerHTML = product[parentId].kcall
+}
+
+const receipt = document.querySelector('.receipt')
+const receiptWindow = document.querySelector('.receipt__window')
+const receiptWindowOut = document.querySelector('.receipt__window-out')
+const receiptWindowBtn = document.querySelector('.receipt__window-btn')
+
+delivery.addEventListener('click', () => {
+    let menu = 'Purchuased: \n'
+    totalPrice = 0
+    kcall = 0
+
+    for (key in product) {
+        for (keyId in product[key]) {
+            if (product[key].amount > 0 && keyId == 'amount') {
+                menu += `${product[key].amount}x ${product[key].name} \n`
+                totalPrice += product[key].sum
+                kcall += product[key].kcall
+            }
+        }
+    }
+    receipt.style.display = 'flex';
+    setTimeout(() => {
+        receipt.style.opacity = '1';
+        receiptWindow.style.top = '0'
+    }, 100)
+
+    document.body.style.overflow = 'hidden'
+
+    receiptWindowOut.innerHTML = `${menu}\n\nCalory: ${kcall}\n\n Total price: ${totalPrice}`
+})
+
+receiptWindowBtn.addEventListener('click', () => location.reload())
+
+
+const sana = document.querySelector(".header__timer-extra");
+recursion()
+function recursion(num = 1, speed = 60) {
+    let time = new Date(),
+        getDat = time.getDate()
+
+    sana.innerHTML = num
+
+    if (num > (getDat/2)) {
+        speed *= 2
+    }
+
+    if (num != getDat) {
+        setTimeout(() => recursion(++num), speed)
+    }
+}
+
+//loader !---------------------------------------------------------------->
+let loader = document.querySelector('.loader_bg');
+
+window.addEventListener('load', () =>{
+    loader.classList.add('hide');
+    setTimeout(() => {
+        loader.remove();
+    }, 600);
+});
